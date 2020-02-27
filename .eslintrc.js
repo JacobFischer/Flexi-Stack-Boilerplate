d6c7ccd7346ebaa88fec405a34dd2173ac19502e
Jacob Fischer <jacob.t.fischer@gmail.com>
2020-02-26 19:52:16 -0600

    Fix incorrect casing in typescript plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,7 +34,7 @@ const baseEslintConfig = {
         "eslint:recommended",
         "plugin:@typescript-eslint/eslint-recommended",
         "plugin:@typescript-eslint/recommended",
-        "plugin:@typeScript-eslint/recommended-requiring-type-checking",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
         "plugin:react/recommended",
         "plugin:import/errors",
         "plugin:import/warnings",
